46415c98a855c11206e4942db0d4a869ff0bf14c
Max Batrak <mbatrak@edenlab.com.ua>
2019-05-24 13:59:24 +0300

    feat: more tasks in task set, some refactoring

--- me/locustfile.py
+++ me/locustfile.py
@@ -9,66 +9,131 @@ import time
 import uuid
 
 
+class SharedData(object):
+  inserted_visits = {}
+  inserted_conditions = {}
+
+
 class MedicalEventsTaskSequence(TaskSequence):
-  task_timeout = 10.0
+  task_timeout = 5.0
+  token = None
   client_id = None
   employee_id = None
   division_id = None
   patient_id = None
   episode_id = None
-  inserted_conditions = []
+  encounter_id = None
   encounter_package = None
-  api_key = "some_api_key"
+  error = False
+  job_url = None
 
   @seq_task(1)
   def create_episode(self):
     self.initTasksData()
     headers = self.login_headers()
 
-    with open("./data/create_episode.json", "r") as json_file:
+    with open("./create_episode.json", "r") as json_file:
       json_data = json.load(json_file)
 
     json_data["id"] = self.episode_id
     json_data["name"] = self.randomString(random.randint(1, 200))
-    json_data["period"]["start"] = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+    json_data["period"]["start"] = datetime.now(
+      timezone.utc).strftime("%Y-%m-%d")
     json_data["managing_organization"]["identifier"]["value"] = self.client_id
     json_data["care_manager"]["identifier"]["value"] = self.employee_id
 
-    self.client.post("/api/patients/{patient_id}/episodes".format(
-      patient_id=self.patient_id), headers=headers, json=json_data)
+    response = self.client.post("/api/patients/{patient_id}/episodes".format(
+      patient_id=self.patient_id), headers=headers, json=json_data, name="create_episode")
+
+    if response.status_code != 202:
+      self.error = True
+    else:
+      response_json = json.loads(response.text)
+      self.job_url = response_json["data"]["links"][0]["href"]
 
   @seq_task(2)
-  def create_encounter_package(self):
-    time.sleep(self.task_timeout)
+  def get_episodes(self):
+    if self.error:
+      return
+
     headers = self.login_headers()
-    encounter_id = str(uuid.uuid4())
+
+    while True:
+      time.sleep(self.task_timeout)
+
+      response = self.client.get("/api{job_url}".format(
+        job_url=self.job_url), headers=headers, name="get_job")
+
+      if response.status_code == 200 or response.status_code == 303:
+        response_json = json.loads(response.text)
+        if response_json["data"]["status"] != "pending":
+          break
+
+    self.client.get("/api/patients/{patient_id}/episodes".format(
+      patient_id=self.patient_id), headers=headers, name="get_episodes")
+
+  @seq_task(3)
+  def get_episode(self):
+    if self.error:
+      return
+
+    headers = self.login_headers()
+
+    self.client.get("/api/patients/{patient_id}/episodes/{episode_id}".format(
+      patient_id=self.patient_id, episode_id=self.episode_id), headers=headers, name="get_episode")
+
+  @seq_task(4)
+  def create_encounter_package(self):
+    if self.error:
+      return
+
+    headers = self.login_headers()
+    self.encounter_id = str(uuid.uuid4())
     visit_id = str(uuid.uuid4())
 
-    with open("./data/create_encounter_package.json", "r") as json_file:
+    with open("./create_encounter_package.json", "r") as json_file:
       json_data = json.load(json_file)
 
-    with open("./data/condition.json", "r") as json_file:
+    with open("./condition.json", "r") as json_file:
       condition_data = json.load(json_file)
 
-    with open("./data/encounter.json", "r") as json_file:
+    with open("./encounter.json", "r") as json_file:
       encounter = json.load(json_file)
 
-    encounter["id"] = encounter_id
+    encounter["id"] = self.encounter_id
     encounter["date"] = datetime.now(timezone.utc).isoformat()
-    encounter["visit"]["identifier"]["value"] = visit_id
     encounter["episode"]["identifier"]["value"] = self.episode_id
     encounter["performer"]["identifier"]["value"] = self.employee_id
     encounter["division"]["identifier"]["value"] = self.division_id
     encounter["prescriptions"] = self.randomString(random.randint(1, 2000))
 
+    reference_previous_visit = bool(random.getrandbits(1))
     reference_previous_condition = bool(random.getrandbits(1))
 
-    if reference_previous_condition and len(self.inserted_conditions) == 0:
+    if self.patient_id not in SharedData.inserted_visits:
+      SharedData.inserted_visits[self.patient_id] = []
+
+    if self.patient_id not in SharedData.inserted_conditions:
+      SharedData.inserted_conditions[self.patient_id] = []
+
+    if reference_previous_visit and len(SharedData.inserted_visits[self.patient_id]) == 0:
+      reference_previous_visit = False
+
+    if reference_previous_condition and len(SharedData.inserted_conditions[self.patient_id]) == 0:
       reference_previous_condition = False
+
+    if len(SharedData.inserted_visits[self.patient_id]) > 200:
+      SharedData.inserted_visits[self.patient_id].pop(0)
+
+    if reference_previous_visit:
+      encounter["visit"]["identifier"]["value"] = random.choice(
+        SharedData.inserted_visits[self.patient_id])
+    else:
+      encounter["visit"]["identifier"]["value"] = visit_id
 
     min_conditions_count = 0
 
-    if len(self.inserted_conditions) == 0 or not reference_previous_condition:
+    if len(SharedData.inserted_conditions[self.patient_id]) == 0 or not reference_previous_condition:
       min_conditions_count = 1
 
     conditions_count = random.randint(min_conditions_count, 3)
@@ -85,48 +150,98 @@ class MedicalEventsTaskSequence(TaskSequence):
       else:
         del condition["asserter"]
 
-      condition["context"]["identifier"]["value"] = encounter_id
+      condition["context"]["identifier"]["value"] = self.encounter_id
       condition["asserted_date"] = datetime.now(timezone.utc).isoformat()
       conditions.append(condition)
-      self.inserted_conditions.append(condition["id"])
 
-      if len(self.inserted_conditions) > 200:
-        self.inserted_conditions.pop(0)
+      if len(SharedData.inserted_conditions[self.patient_id]) > 200:
+        SharedData.inserted_conditions[self.patient_id].pop(0)
 
     if reference_previous_condition:
       encounter["diagnoses"][0]["condition"]["identifier"]["value"] = random.choice(
-        self.inserted_conditions)
+        SharedData.inserted_conditions[self.patient_id])
     else:
       encounter["diagnoses"][0]["condition"]["identifier"]["value"] = random.choice(conditions)[
         "id"]
 
     self.encounter_package = {"encounter": encounter, "conditions": conditions}
-    signed_data = base64.b64encode(json.dumps(self.encounter_package).encode("ascii"))
+    signed_data = base64.b64encode(json.dumps(
+      self.encounter_package).encode("ascii"))
     json_data["visit"]["id"] = visit_id
     json_data["signed_data"] = signed_data.decode("ascii")
 
-    self.client.post("/api/patients/{patient_id}/encounter_package".format(
-      patient_id=self.patient_id), headers=headers, json=json_data)
+    response = self.client.post("/api/patients/{patient_id}/encounter_package".format(
+      patient_id=self.patient_id), headers=headers, json=json_data, name="create_encounter_package")
 
-  @seq_task(3)
+    if response.status_code == 202:
+      response_json = json.loads(response.text)
+      self.job_url = response_json["data"]["links"][0]["href"]
+
+      while True:
+        time.sleep(self.task_timeout)
+
+        response = self.client.get("/api{job_url}".format(
+          job_url=self.job_url), headers=headers, name="get_job")
+
+        if response.status_code == 200 or response.status_code == 303:
+          response_json = json.loads(response.text)
+          if response_json["data"]["status"] == "processed":
+            SharedData.inserted_visits[self.patient_id].append(visit_id)
+
+            for x in range(conditions_count):
+              SharedData.inserted_conditions[self.patient_id].append(
+                condition["id"])
+
+          if response_json["data"]["status"] != "pending":
+            break
+
+    if response.status_code != 202:
+      self.error = True
+
+  @seq_task(5)
   def update_episode(self):
+    if self.error:
+      return
+
     headers = self.login_headers()
 
-    with open("./data/update_episode.json", "r") as json_file:
+    with open("./update_episode.json", "r") as json_file:
       json_data = json.load(json_file)
 
     json_data["name"] = self.randomString(random.randint(1, 200))
     json_data["care_manager"]["identifier"]["value"] = self.employee_id
 
     self.client.patch("/api/patients/{patient_id}/episodes/{episode_id}".format(
-      patient_id=self.patient_id, episode_id=self.episode_id), headers=headers, json=json_data)
+      patient_id=self.patient_id, episode_id=self.episode_id), headers=headers, json=json_data, name="update_episode")
 
-  @seq_task(4)
-  def cancel_encounter_package(self):
-    time.sleep(self.task_timeout)
+  @seq_task(6)
+  def get_encounters(self):
+    if self.error:
+      return
+
     headers = self.login_headers()
 
-    with open("./data/cancel_encounter_package.json", "r") as json_file:
+    self.client.get("/api/patients/{patient_id}/episodes/{episode_id}/encounters".format(
+      patient_id=self.patient_id, episode_id=self.episode_id), headers=headers, name="get_encounters")
+
+  @seq_task(7)
+  def get_encounter(self):
+    if self.error:
+      return
+
+    headers = self.login_headers()
+
+    self.client.get("/api/patients/{patient_id}/episodes/{episode_id}/encounters/{encounter_id}".format(
+      patient_id=self.patient_id, episode_id=self.episode_id, encounter_id=self.encounter_id), headers=headers, name="get_encounter")
+
+  @seq_task(8)
+  def cancel_encounter_package(self):
+    if self.error:
+      return
+
+    headers = self.login_headers()
+
+    with open("./cancel_encounter_package.json", "r") as json_file:
       json_data = json.load(json_file)
 
     self.encounter_package["encounter"]["cancellation_reason"] = {
@@ -139,33 +254,60 @@ class MedicalEventsTaskSequence(TaskSequence):
     for x in range(conditions_count):
       self.encounter_package["conditions"][x]["verification_status"] = "entered_in_error"
 
-    signed_data = base64.b64encode(json.dumps(self.encounter_package).encode("ascii"))
+    signed_data = base64.b64encode(json.dumps(
+      self.encounter_package).encode("ascii"))
     json_data["signed_data"] = signed_data.decode("ascii")
 
     self.client.patch("/api/patients/{patient_id}/encounter_package".format(
-      patient_id=self.patient_id), headers=headers, json=json_data)
+      patient_id=self.patient_id), headers=headers, json=json_data, name="cancel_encounter_package")
 
-  @seq_task(5)
+  @seq_task(9)
   def close_episode(self):
+    if self.error:
+      return
+
     headers = self.login_headers()
 
-    with open("./data/close_episode.json", "r") as json_file:
+    with open("./close_episode.json", "r") as json_file:
       json_data = json.load(json_file)
 
-    json_data["period"]["end"] = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+    json_data["period"]["end"] = datetime.now(
+      timezone.utc).strftime("%Y-%m-%d")
     json_data["closing_summary"] = self.randomString(random.randint(1, 2000))
 
     self.client.patch("/api/patients/{patient_id}/episodes/{episode_id}/actions/close".format(
-      patient_id=self.patient_id, episode_id=self.episode_id), headers=headers, json=json_data)
+      patient_id=self.patient_id, episode_id=self.episode_id), headers=headers, json=json_data, name="close_episode")
 
-  # Helpers
+  @seq_task(10)
+  def get_conditions(self):
+    if self.error:
+      return
+
+    headers = self.login_headers()
+
+    self.client.get("/api/patients/{patient_id}/conditions".format(
+      patient_id=self.patient_id), headers=headers, name="get_conditions")
+
+  @seq_task(11)
+  def get_condition(self):
+    if self.error:
+      return
+
+    headers = self.login_headers()
+    condition_id = SharedData.inserted_conditions[self.patient_id][len(
+      SharedData.inserted_conditions[self.patient_id]) - 1]
+
+    self.client.get("/api/patients/{patient_id}/conditions/{condition_id}".format(
+      patient_id=self.patient_id, condition_id=condition_id), headers=headers, name="get_condition")
+
   def initTasksData(self):
-    with open("./data/context_data.txt", "r") as txt_file:
-      lines = txt_file.read().splitlines()
-      line = random.choice(lines)
-      [self.client_id, self.employee_id, self.division_id,
-       self.patient_id, self.token] = line.split(";")
-      self.episode_id = str(uuid.uuid4())
+    with open("./context_data.txt", "r") as context_data_file:
+      context_data = context_data_file.read().splitlines()
+
+    context = random.choice(context_data)
+    [self.client_id, self.employee_id, self.division_id,
+     self.patient_id, self.token] = context.split(";")
+    self.episode_id = str(uuid.uuid4())
 
   def randomString(self, stringLength=10):
     letters = string.ascii_lowercase
@@ -175,12 +317,11 @@ class MedicalEventsTaskSequence(TaskSequence):
     return {
       "x-consumer-id": str(uuid.uuid4()),
       "Authorization": "Bearer {token}".format(token=self.token),
-      "api-key": self.api_key
+      "api-key": self.locust.api_key
     }
 
 
 class WebsiteUser(HttpLocust):
   host = "http://localhost:4000"
+  api_key = "some_api_key"
   task_set = MedicalEventsTaskSequence
-  min_wait = 2000
-  max_wait = 9000
