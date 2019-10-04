d87105a774f6c9cb9d17cdbb373b393e8569511b
Maksym Ivanov <imax.iva@gmail.com>
2019-10-04 11:38:54 +0300

    feat: mithril verify token only locust file created

--- mithril/locustfile_login_verify.py
+++ mithril/locustfile_login_verify.py
@@ -1,15 +1,7 @@
 from locust import HttpLocust, TaskSet, task
+from settings import *
 from itertools import cycle
 import csv
-
-
-config = {
-    'DEV': {
-        'AUTH_CLIENT_ID': 'e2e1d2c8-9bac-43c6-adef-83239940b30a',
-        'MIS_API_KEY': 'b1lPV3d3blRHRHdOYVYrYmNPU0tJdz09'
-    }
-}
-environment = 'DEV'
 
 
 def load_user_params():
---
+++ mithril/settings.py
@@ -0,0 +1,8 @@
+config = {
+    'DEV': {
+        'AUTH_CLIENT_ID': 'e2e1d2c8-9bac-43c6-adef-83239940b30a',
+        'MIS_API_KEY': 'b1lPV3d3blRHRHdOYVYrYmNPU0tJdz09'
+    }
+}
+
+environment = 'DEV'
